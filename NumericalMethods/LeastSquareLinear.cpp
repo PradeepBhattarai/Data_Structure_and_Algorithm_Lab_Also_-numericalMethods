ee766c3bc26fa94d0c6998f7fb6e6d0d541f7913
Pradeep Bhattarai <pradeepbhattarai08@gmail.com>
2023-02-01 17:51:53 +0545

    I don't know

---
+++ NumericalMethods/LeastSquareLinear.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of datas:"<<endl;
+    cin>>n;
+    cout<<"Enter the values of x and y:"
+    float x[n],y[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>x[n]>>y[n];
+    }
+    
+    return 0;
+}
